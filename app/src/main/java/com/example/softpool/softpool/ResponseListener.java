5e4cc2a5c8f9ff032a9b7baa879297e7ebf4ab28
L31T40 <jorgeleitao76@gmail.com>
2020-02-29 17:18:53 +0000

    security fixes

--- app/src/main/java/com/example/softpool/softpool/ResponseListener.java
+++
@@ -1,8 +0,0 @@
-package com.example.softpool.softpool;
-
-/**
- * Created by Jorge leitão on 25/06/2018.
- **/
-public interface ResponseListener {
-    void onResponseReceive(String data); //aguarda resposta/termino da asynctask e recebe o valor delovido
-}
